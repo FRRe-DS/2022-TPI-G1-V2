f35474269526440bcb178e379717373aca7455d4
ianklebold <ianstgo@gmail.com>
2022-05-25 15:15:41 -0300

    EUREKA - 001 - EUREKA IS READY

--- matches-service/src/main/java/com/match/matches/MatchesApplication.java
+++ matches-service/src/main/java/com/match/matches/MatchesApplication.java
@@ -2,7 +2,10 @@ package com.match.matches;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class MatchesApplication {
 
--- travel-service/src/main/java/com/api/service/travel/TravelApplication.java
+++ travel-service/src/main/java/com/api/service/travel/TravelApplication.java
@@ -2,7 +2,10 @@ package com.api.service.travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class TravelApplication {
 
