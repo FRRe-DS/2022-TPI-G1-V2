b90bad5dbe5abf89c6cd7efe4714d570f26ed28a
ianklebold <ianstgo@gmail.com>
2022-06-26 01:30:45 -0300

    Oauth - 000 - Added dependencies

---
+++ oauth-service/src/main/java/com/oauth/oauthservice/OauthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oauth.oauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthServiceApplication.class, args);
+	}
+
+}
