72d466a3b48718fd580dabd7e511ad3f925cf9ac
ianklebold <ianstgo@gmail.com>
2022-06-26 01:50:37 -0300

    ADDED user-commons

---
+++ user-commons/src/main/java/com/usercommons/usercommons/UserCommonsApplication.java
@@ -0,0 +1,11 @@
+package com.usercommons.usercommons;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class UserCommonsApplication {
+
+}
---
+++ user-commons/src/test/java/com/usercommons/usercommons/UserCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usercommons.usercommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
